5d8c83c4cb327f05f9a2d55fb1ff64b338cb07a4
amanmdesai <amanmukeshdesai@gmail.com>
2026-03-14 10:53:22 +1030

    update test

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -1,19 +1,28 @@
 import pygrc
 import numpy as np
-import random
 import math
 
-def function(x, *args):
-    return args[0]*x + args[1]
 
-# this will the least square against two numpy arrays
+def function(x, *args):
+    return args[0] * x + args[1]
+
+
+# Least-squares fit test against synthetic data
 def test_fit_lsq():
-    x = np.linspace(0,10,10)
-    y = np.zeros(10)
-    for i in range(len(y)):
-        y[i] = 2*x[i] + 2*random.random()
-    m=pygrc.Fit(x, y, 1,1).fit_lsq(function,[(-1,10),(-2,10)],.1)
-    assert math.fabs(m.values["x0"]-2.0) <  0.1, \
-        "Least Square x0 parameter does not converge to true value"
-    assert math.fabs(m.values["x1"]-1.0) <  1., \
-        "Least Square x1 parameter does not converge to true value"
+    rng = np.random.default_rng(42)
+
+    x = np.linspace(0, 10, 10)
+    noise = rng.random(10)
+    y = 2 * x + noise
+
+    m = pygrc.Fit(x, y, 1, 1).fit_lsq(
+        function,
+        [(-1, 10), (-2, 10)],
+        0.1
+    )
+
+    assert math.fabs(m.values["x0"] - 2.0) < 0.1, \
+        "Least Square slope (x0) did not converge to true value"
+
+    assert math.fabs(m.values["x1"] - 0.5) < 1.0, \
+        "Least Square intercept (x1) not within reasonable range"
